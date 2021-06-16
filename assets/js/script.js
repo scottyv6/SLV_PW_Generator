08977fd458118f520cf49b0234b78d8754d73994
scottyv <scottyv6@gmail.com>
2021-06-16 16:18:26 +0800

    Updated js file all input functions completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,11 +20,39 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 function generatePassword() {
-  while (pwLength <8 || pwLength >128) {
+  pwLength = getPWLength();
+
+  do {
+    var isLower = isCharsetUsed("lowercase letters");
+    var isUpper = isCharsetUsed("uppercase letters");
+    var isNumeric = isCharsetUsed("numbers");
+    var isSpecial = isCharsetUsed("special characters");
+
+    if (!(isLower || isUpper || isNumeric || isSpecial)) {
+      window.alert("You must choose at least 1 set of characters!")
+    }
+  } 
+  while (!(isLower || isUpper || isNumeric || isSpecial));
+  
+
+  /* testing
+  window.alert("lower is " + isLower);
+  window.alert("upper is " + isUpper);
+  window.alert("numeric is " + isNumeric);
+  window.alert("special is " + isSpecial);
+  */
+  return("12345678");
+}
+
+/* This function prompts a user for the length of their password.
+   It checks the user has entered a valid number in the range and 
+   returns an integer */
+function getPWLength() {
+  do {
     pwLength = window.prompt("How long is your password? \r\nPlease enter a number between 8 and 128, inclusive.");
     if (pwLength === null) {
       window.alert("Bye");
@@ -37,8 +65,18 @@ function generatePassword() {
     else if (pwLength <8 || pwLength >128) {
       window.alert("You must enter a number between 8 and 128, inclusive. ");
     }
+    
     //window.alert("length is " + pwLength);
-  }
+    //make sure pwLength is an integer
+  } while (pwLength <8 || pwLength >128);
+  pwLength = parseInt(pwLength);
+  return (pwLength);
+}
+/*This function prompts user if they want to use a character set
+  and returns a boolean */
+function isCharsetUsed(charSet) {
+  var isUsed = window.confirm("Do you want to include " + charSet + " in you password?");
+  return(isUsed);
 }
 
 // Add event listener to generate button
